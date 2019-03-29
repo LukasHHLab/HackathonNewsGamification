7aed368da2c5f44390e4f8c44bc9d915c4a7a579
Lukas Wilkes <lukas.wilkes@hhlab.de>
2019-03-29 17:21:50 +0100

    store user object in local storage.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
-let user = {
-    "username": "Max Mustermann",
-    "score": 100,
-    "badges": [{
-        "badgeName": "Entdecker",
-        "badgeDescription": "Beende deine erste IStory.",
-        "earnedAt": "28.03.2019 12:55"
-      },
-      {
-        "badgeName": "Lokalheld",
-        "badgeDescription": "Lies fünf lokale IStories.",
-        "earnedAt": "29.03.2019 14:09"
-      }
-    ]
-  };
 
   $(document).ready(function () {
+    if(localStorage.getItem("user") === null){
+        initUser("Max Muster");
+    }
+    var user = loadUser();
+
     reloadScoreView();
     reloadBadgesView();
     $(".js-profile-username").text(user.username);
   });
 
+  function initUser(username) {
+      user = {
+        "username": username,
+        "score": 0,
+        "badges": [{
+            "badgeName": "Entdecker",
+            "badgeDescription": "Beende deine erste IStory.",
+            "earnedAt": "28.03.2019 12:55"
+          }
+        ]
+      };
+
+      saveUser(user);
+  }
+
+  function loadUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+
+  function saveUser(user) {
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
   function reloadScoreView() {
+    var user = loadUser();
     $(".js-user-score").text(user.score);
     $(".js-profile-score").text(user.score);
   }
 
   function reloadBadgesView() {
+    var user = loadUser();
     $(".js-profile-badges").empty();
 
     for (var i in user.badges) {
@@ -37,22 +52,26 @@ let user = {
 
   function toggleProfileModal() {
     $(".js-modal-profile").slideToggle();
-    $(".js-modal-profile").toggleClass("active");
+    $(".js-user").toggleClass("active");
     $(".js-content").slideToggle();
     reloadScoreView();
   }
 
   function increaseUserScore(value) {
+    var user = loadUser();
     user.score += value;
+    saveUser(user);
     reloadScoreView();
   }
 
   function addBadge(badgeName, earnedAt, badgeDescription) {
+    var user = loadUser();
     user.badges.push({
       "badgeName": badgeName,
       "badgeDescription": badgeDescription,
       "earnedAt": earnedAt
     });
+    saveUser(user);    
 
     reloadBadgesView();
   }
