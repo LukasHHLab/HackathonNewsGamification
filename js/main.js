ff51637708ef9a3d5d61a0dcc2de6a9431e78eb1
Marco Kreienbrink <m.kreienbrink@basecom.de>
2019-03-29 18:15:48 +0100

    swipe/tap

--- js/main.js
+++ js/main.js
@@ -5,64 +5,64 @@ let user = {
         "badgeName": "Entdecker",
         "badgeDescription": "Beende deine erste IStory.",
         "earnedAt": "28.03.2019 12:55"
-      },
-      {
-        "badgeName": "Lokalheld",
-        "badgeDescription": "Lies fünf lokale IStories.",
-        "earnedAt": "29.03.2019 14:09"
-      }
+    },
+        {
+            "badgeName": "Lokalheld",
+            "badgeDescription": "Lies fünf lokale IStories.",
+            "earnedAt": "29.03.2019 14:09"
+        }
     ]
-  };
+};
 
-  $(document).ready(function () {
+$(document).ready(function () {
     reloadScoreView();
     reloadBadgesView();
     $(".js-profile-username").text(user.username);
-  });
+});
 
-  function reloadScoreView() {
+function reloadScoreView() {
     $(".js-user-score").text(user.score);
     $(".js-profile-score").text(user.score);
-  }
+}
 
-  function reloadBadgesView() {
+function reloadBadgesView() {
     $(".js-profile-badges").empty();
 
     for (var i in user.badges) {
-      $(".js-profile-badges").append("<li>" + user.badges[i].badgeName + " <small>" + user.badges[i].earnedAt +
-        "</small></li>");
+        $(".js-profile-badges").append("<li>" + user.badges[i].badgeName + " <small>" + user.badges[i].earnedAt +
+            "</small></li>");
     }
 
-  }
+}
 
-  function toggleProfileModal() {
+function toggleProfileModal() {
     $(".js-modal-profile").slideToggle();
     $(".js-modal-profile").toggleClass("active");
     $(".js-content").slideToggle();
     reloadScoreView();
-  }
+}
 
-  function increaseUserScore(value) {
+function increaseUserScore(value) {
     user.score += value;
     reloadScoreView();
-  }
+}
 
-    function addBadge(badgeName, earnedAt, badgeDescription) {
-        user.badges.push({
-            "badgeName": badgeName,
-            "badgeDescription": badgeDescription,
-            "earnedAt": earnedAt
-        });
+function addBadge(badgeName, earnedAt, badgeDescription) {
+    user.badges.push({
+        "badgeName": badgeName,
+        "badgeDescription": badgeDescription,
+        "earnedAt": earnedAt
+    });
 
-        reloadBadgesView();
-    }
+    reloadBadgesView();
+}
 
-    $(document).ready(function () {
+$(document).ready(function () {
     var myElement = $('article.active')[0];
     var mc = new Hammer(myElement);
 
-    mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-    mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+    mc.get('pan').set({direction: Hammer.DIRECTION_ALL});
+    mc.get('swipe').set({direction: Hammer.DIRECTION_ALL});
 
     var previousArticle = function () {
         var curr = $('article.active');
