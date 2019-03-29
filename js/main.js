8c0b6ea0665623e9f72a6cb959c2deac2ab91b1f
Marco Kreienbrink <m.kreienbrink@basecom.de>
2019-03-29 17:20:37 +0100

    swipe/tap

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 $(document).ready(function () {
     var myElement = $('.active')[0];
     var mc = new Hammer(myElement);
+
     mc.get('pan').set({ direction: Hammer.DIRECTION_ALL });
     mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 
-    mc.on("panleft panright panup pandown tap press", function (ev) {
-        myElement.textContent = ev.type +" gesture detected.";
-    });
-    mc.on("swipeleft swiperight swipeup swipedown tap press", function (ev) {
-        myElement.textContent = ev.type +" gesture detected.";
+    var previousArticle = function () {
+        var curr = $('article.active');
+        curr.removeClass('active');
+        var prev = curr.prev();
+        if (!prev.length) {
+            prev = $('article').last();
+        }
+        prev.addClass('active');
+    };
+
+    var nextArticle = function () {
+        var curr = $('article.active');
+        curr.removeClass('active');
+        var next = curr.next();
+        if (!next.length) {
+            next = $('article').first();
+        }
+
+        next.addClass('active');
+    };
+
+    var nextSection = function () {
+        var curr = $('article section.active');
+        var article = curr.closest('article');
+        curr.removeClass('active');
+        var next = curr.next();
+        if (!next.length) {
+            next = $(article).find('section').first();
+        }
+
+        next.addClass('active');
+    };
+
+    mc.on("panleft panright tap press", function (ev) {
+        switch (ev.type) {
+            case "panleft":
+                previousArticle();
+                break;
+            case "panright":
+                nextArticle();
+                break;
+            case "tap":
+            case "press":
+                nextSection();
+                break;
+        }
     });
 });
